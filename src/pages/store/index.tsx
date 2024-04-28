2f715c7770646d444d4c05a860626839d6fe6dd1
Vandivier <vandivi3r@gmail.com>
2024-04-28 13:20:51 -0400

    feat: simpler store page

--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -1,88 +1,29 @@
 import { signIn, useSession } from "next-auth/react";
 import { CustomPage } from "~/components/CustomPage";
 import { SubheadingWithDice } from "~/components/SubheadingWithDice";
-import { api } from "~/utils/api";
 
 export default function StorePage() {
-  const hello = api.post.hello.useQuery({ text: "from Aria's Tale!" });
+  const { data: sessionData } = useSession();
 
   return (
-    <CustomPage mainHeading="Embark on an Epic Journey with Aria's Tale!">
+    <CustomPage mainHeading="Aria's Tale Store">
       <>
-        <SubheadingWithDice
-          subheading={hello.data ? hello.data.greeting : "Loading..."}
-        />
-        <AuthButtonIfNeeded />
-
-        <div className="text-l my-4 flex flex-col gap-8 text-white">
-          <p>Join our vibrant community of adventurers and shape the story!</p>
-          <p>
-            Read the living narrative of Aria's Tale{" "}
-            <a
-              className="underline"
-              href="https://www.ariastale.com/game-manual/narrative"
-            >
-              here
-            </a>
-            , get started playing now with the{" "}
-            <a
-              className="underline"
-              href="https://www.ariastale.com/game-manual/quick-start"
-            >
-              Quick Start Guide
-            </a>
-            , or dive into the technical details with the{" "}
-            <a
-              className="underline"
-              href="https://www.ariastale.com/game-manual/metagame"
-            >
-              Game Manual
-            </a>
-            .
-          </p>
-          <p>
-            This website is in development. In the future, you will be able to
-            play Aria's Tale through this website. For now, Aria's Tale is
-            played over social media and during periodic streams, as mentioned
-            in the{" "}
-            <a
-              className="underline"
-              href="https://www.ariastale.com/game-manual/quick-start"
-            >
-              Quick Start Guide
-            </a>
-            .
-          </p>
-          <p>
-            Join the waitlist to be notified when the game is ready to play
-            through this site, and to indicate whether you are interested in
-            joining a live stream!
-          </p>
-        </div>
+        <SubheadingWithDice subheading={`Purchase Aria's Tale TCG Cards`} />
+        {sessionData ? (
+          <p>nice job being logged in bro</p>
+        ) : (
+          <AuthButtonIfNeeded />
+        )}
       </>
     </CustomPage>
   );
 }
 
-function AuthButtonIfNeeded() {
-  const { data: sessionData } = useSession();
-
-  return sessionData ? (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        <span>
-          You are logged in! Nice! In the future, you can buy a card here.
-        </span>
-      </p>
-
-      {!sessionData && (
-        <button
-          className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-          onClick={void signIn()}
-        >
-          "Sign in or join now!"
-        </button>
-      )}
-    </div>
-  ) : null;
-}
+const AuthButtonIfNeeded = () => (
+  <button
+    className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+    onClick={() => void signIn()}
+  >
+    Sign in or join now to make a purchase!
+  </button>
+);
