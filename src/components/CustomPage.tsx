a03f8dbff0e52899161aacd94a7a0a804017ce15
Vandivier <vandivi3r@gmail.com>
2024-06-29 01:28:26 -0400

    style: page gutters

--- src/components/CustomPage.tsx
+++ src/components/CustomPage.tsx
@@ -61,7 +61,7 @@ export const CustomPage = ({
 
       {contentBeforeTitle && contentBeforeTitle}
 
-      <div className="container flex flex-col items-center gap-6 px-4 py-2 md:py-8">
+      <div className="container m-auto flex w-full flex-col items-center gap-6 px-4 py-2 md:w-1/2 md:py-8">
         <h1 className="text-2xl font-extrabold tracking-tight text-white sm:text-[4rem] md:leading-[4rem]">
           {mainHeading}
         </h1>
