8c987994ed5f67ffe4d637145ad692e7cd9facb6
Vandivier <vandivi3r@gmail.com>
2024-08-25 15:19:10 -0400

    feat: allow custom headings

--- src/components/CustomPage.tsx
+++ src/components/CustomPage.tsx
@@ -5,7 +5,7 @@ import TopNav from "~/components/TopNav";
 interface CustomPageProps {
   title?: string;
   metaDescription?: string;
-  mainHeading: string;
+  mainHeading: string | React.ReactNode;
   children: React.ReactNode;
   contentBeforeTitle?: React.ReactNode;
 }
@@ -61,10 +61,14 @@ export const CustomPage = ({
 
       {contentBeforeTitle && contentBeforeTitle}
 
-      <div className="container m-auto flex w-full flex-col items-center gap-6 px-4 py-2 md:w-1/2 md:py-8">
-        <h1 className="text-2xl font-extrabold tracking-tight text-white sm:text-[4rem] md:leading-[4rem]">
-          {mainHeading}
-        </h1>
+      <div className="container m-auto my-4 flex w-full flex-col items-center gap-6 px-4 py-2 md:w-1/2 md:py-8">
+        {typeof mainHeading === "string" ? (
+          <h1 className="text-xl font-extrabold tracking-tight text-white sm:text-[4rem] md:leading-[4rem]">
+            {mainHeading}
+          </h1>
+        ) : (
+          mainHeading
+        )}
 
         <div className="text-l flex max-w-full flex-col items-center gap-2 text-white md:text-xl">
           {children}
